3936feb3bf9b65a8ac17df05927669ea23550b4b
Lavender-Townhall <lavenderhushed@e-record.com>
2025-01-29 08:15:29 +0700

    Create sense.js

---
+++ sense.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6874412455331618";
+  script.async = true;
+  script.crossOrigin = "anonymous";
+  document.head.insertBefore(script, document.head.firstChild);
+})();
